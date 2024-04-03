e0ea70e95d9406e437058faad6653596a93deddf
kkaengchoon <77263204+kkaengchoon@users.noreply.github.com>
2024-04-03 21:27:55 +0900

    Add files via upload

--- ch03_fakechatGPT.py
+++ ch03_fakechatGPT.py
@@ -145,11 +145,10 @@ def main():
             # 채팅 형식으로 시각화 하기
             for sender, time, message in st.session_state["chat"]:
                 if sender == "user":
-                    st.write(f'<div style="display:flex;align-items:center;"><div style="background-color:#007AFF;color:white;border-radius:12px;padding:8px 12px;margin-right:8px;">{message}</div><div style="font-size:0.8rem;color:gray;">{time}</div></div>', unsafe_allow_html=True)
-                    st.write("")
+                    st.write(f'<div style="display:flex;align-items:center;"><div style="background-color:green;color:white;border-radius:12px;padding:8px 8px;margin-right:8px;">{message}</div><div style="font-size:0.8rem;color:gray; >{time}</div></div>', unsafe_allow_html=True)
+                    
                 else:
-                    st.write(f'<div style="display:flex;align-items:center;justify-content:flex-end;"><div style="background-color:lightgray;border-radius:12px;padding:8px 12px;margin-left:8px;">{message}</div><div style="font-size:0.8rem;color:gray;">{time}</div></div>', unsafe_allow_html=True)
-                    st.write("")
+                    st.write(f'<div style="display:flex;align-items:center;justify-content:flex-end width = "30px";"><div style="background-color:blue;border-radius:12px;padding:4px 8px;margin-left:8px;">{message}</div><div style="font-size:0.8rem;color:gray;">{time}</div></div>', unsafe_allow_html=True)
 
 if __name__=="__main__":
     main()
